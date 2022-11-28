4f40c00fb28925f076e82f828a6192a834d1f918
karolinaklak <karolinaklak@live.fr>
2022-11-28 08:49:18 +0100

    First commit. Basic Telegram Bot greeting user.

---
+++ bot.rb
@@ -0,0 +1,25 @@
+require 'telegram_bot'
+token ='5820461848:AAGK2TlC42yzei18tolGS7hVmKdHkhxyKmk'
+bot = TelegramBot.new(token: token)
+
+
+bot.get_updates(fail_silently: true) do |message|
+    puts "@#{message.from.username}: #{message.text}"
+    command = message.get_command_for(bot)
+
+    message.reply do |reply|
+        case command
+        when /start/i
+            reply.text = "Hi. Hope you doing well. All I can do is say hello. Please, try the /greet or /salut command"
+        when /greet/i
+            reply.text = "Hello, #{message.from.first_name}"
+        when /salut/i
+            reply.text = "Salut, #{message.from.first_name}"
+            
+        else
+            reply.text = "I have no idea what #{command.inspect} means."
+        end
+        puts "sending #{reply.text.inspect} to @#{message.from.username}"
+        reply.send_with(bot)
+    end
+end
